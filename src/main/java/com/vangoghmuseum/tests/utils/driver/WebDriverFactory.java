56c1a2dd7daf91e7b6b119e1c541309fa88a1828
Anna Moshchenok <Anna_Moshchenok@neimanmarcus.com>
2022-05-15 18:20:46 +0200

    updated driver

--- src/main/java/com/vangoghmuseum/tests/utils/driver/WebDriverFactory.java
+++ src/main/java/com/vangoghmuseum/tests/utils/driver/WebDriverFactory.java
@@ -4,16 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.vangoghmuseum.tests.utils.TestConfig.testConfigurations;
 
 public class WebDriverFactory {
     private WebDriver driver;
 
     public WebDriverFactory() {
-
+        ChromeOptions options = new ChromeOptions();
         switch (testConfigurations.os()) {
             case "mac":
                 System.setProperty("webdriver.chrome.driver", "driver/chrome/macOs/chromedriver");
@@ -21,20 +18,19 @@ public class WebDriverFactory {
 
             case "win":
                 System.setProperty("webdriver.chrome.driver", "driver/chrome/win/chromedriver.exe");
+                options.addArguments("--disable-gpu");
                 break;
             case "linux":
                 System.setProperty("webdriver.chrome.driver", "driver/chrome/linux/chromedriver");
+                options.addArguments("--headless");
                 break;
             default:
                 throw new IllegalArgumentException(
                         String.format("There is no test os implementation for: '%s'", testConfigurations.os()));
         }
-        Map<String, Object> chromePrefs = new HashMap<>();
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("--test-type");
-        options.addArguments("--disable-extensions"); //to disable browser extension popup
-        this.driver = new ChromeDriver();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--window-size=1920,1080");
+        this.driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
